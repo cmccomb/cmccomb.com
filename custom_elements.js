7abf41e9eacd151197719adc432a08fffe346ac9
cmccomb <ccmcc2012@gmail.com>
2015-06-01 11:48:57 -0400

    Cleanup for custom elements.

---
+++ custom_elements.js
@@ -0,0 +1,9 @@
+document.registerElement('pub-year');
+document.registerElement('pub-author');
+document.registerElement('pub-title');
+document.registerElement('pub-journal');
+document.registerElement('pub-volume');
+document.registerElement('pub-pages');
+document.registerElement('pub-conference');
+document.registerElement('pub-doi');
+document.registerElement('pub-pdf');
